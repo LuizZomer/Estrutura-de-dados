b05d77762e474bde9389663febc6f054d85bde2e
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-09-20 14:16:31 -0300

    Update lista_encadeada.py

--- lista_encadeada/lista_encadeada.py
+++ lista_encadeada/lista_encadeada.py
@@ -19,14 +19,70 @@ class ListaEncadeada:
         self.primeiro = novo
     
     def mostrar(self):
+        if self.primeiro == None:
+            print("A lista está vazia")
         atual = self.primeiro
         while atual != None:
             atual.mostrar_no()
             atual = atual.proximo
 
 
+    def pesquisa(self, valor):
+        if self.primeiro == None:
+            print("A lista está vazia")
+            return None
+        
+        atual = self.primeiro
+        while atual.valor != valor:
+            if atual.proximo == None:
+                return None
+            atual = atual.proximo
+        return atual
+
+    
+    def excluir_inicio(self):
+        if self.primeiro == None:
+            print("A lista está vazia")
+            return None
+        temp = self.primeiro
+        self.primeiro = self.primeiro.proximo
+        return temp
+
+
+    def excluir_posicao(self, valor):
+        if self.primeiro == None:
+            print("A lista está vazia")
+            return None
+        atual = self.primeiro
+        anterior = self.primeiro
+        while atual.valor != valor:
+            if atual.proximo == None:
+                return None
+            anterior = atual
+            atual = atual.proximo
+        if atual == self.primeiro:
+            self.excluir_inicio()
+        else:
+            anterior.proximo = atual.proximo
+        
+        return atual
+
+
 lista = ListaEncadeada()
 lista.inserir_inicio(1)
+lista.inserir_inicio(2)
 lista.inserir_inicio(3)
-lista.primeiro
+lista.inserir_inicio(4)
+# lista.mostrar()
+# lista.excluir_inicio()
+# lista.excluir_inicio()
+# lista.mostrar()
+# pesquisa = lista.pesquisa(3)
+lista.excluir_posicao(2)
 lista.mostrar()
+
+# if pesquisa != None:
+#     print(f"Valor encotrado: {pesquisa.valor}")
+
+# else:
+#     print('Valor não encontrado')
